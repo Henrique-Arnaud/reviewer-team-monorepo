0873e4becbf43524d823afa0585470ed39771c20
Henrique Arnaud <henrique.ass@einstein.br>
2025-06-13 14:14:53 -0300

    feat: project separeted into 2 microfronts inside a monorep

---
+++ apps/auth-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { useAuth } from 'auth-hooks'
+
+export default function Home() {
+  const {} = useAuth()
+  return (
+    <div className="absolute top-1">
+
+    </div>
+  );
+}
---
+++ packages/auth-hooks/src/useAuth.ts
@@ -0,0 +1,6 @@
+// packages/auth-hooks/useAuth.ts
+import Cookies from 'js-cookie'
+export const useAuth = () => {
+  const token = Cookies.get('token')
+  return { isAuthenticated: !!token }
+}
